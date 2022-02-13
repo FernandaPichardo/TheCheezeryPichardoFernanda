d7e1b3de158dad3b3085ff21165688d25c9263e6
Fernanda <fpichardop@gmail.com>
2022-02-13 17:09:16 -0600

    First commit

---
+++ app/src/main/java/pichardo/fernanda/thecheezery/MenuActivity.kt
@@ -0,0 +1,11 @@
+package pichardo.fernanda.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
